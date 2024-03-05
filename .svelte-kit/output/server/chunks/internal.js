dc0fc2b14c62d6d42f5a50913eca4ad3102a5dfd
Sean Isayama <hikaruisayama@Seans-MacBook-Pro-2.local>
2024-03-04 17:42:49 -0800

    update github pages

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -190,7 +190,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "141ml6e"
+  version_hash: "1kvafej"
 };
 async function get_hooks() {
   return {};
